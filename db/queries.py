1e766a8a475370e490fc5842622ad53991d1736b
marlenlab <marlenmarlisovich@gmail.com>
2026-05-22 12:39:35 +0000

    added HW 7

--- db/queries.py
+++ db/queries.py
@@ -1,20 +1,33 @@
 create_tasks_table = '''
 CREATE TABLE IF NOT EXISTS tasks (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    task TEXT NOT NULL        
+    task TEXT NOT NULL,
+    completed INTEGER DEFAULT 0
 )
 '''
-# pip install flet[all]
-# CRUD - CREATE READ UPDATE DELETE
 
 # CREATE
-insert_task ='INSERT INTO tasks (task) VALUES (?)'
+insert_task = 'INSERT INTO tasks (task) VALUES (?)'
 
 # READ
-select_tasks ="SELECT id, task FROM tasks"
+select_tasks = "SELECT id, task, completed FROM tasks"
+
+select_tasks_completed = """
+SELECT id, task, completed FROM tasks
+WHERE completed = 1
+"""
+
+select_tasks_uncompleted = """
+SELECT id, task, completed FROM tasks
+WHERE completed = 0
+"""
 
 # UPDATE
-update_task = "UPDATE tasks SET task = ? WHERE id = ? "
+update_task = "UPDATE tasks SET task = ? WHERE id = ?"
 
-# UPDATE
+# DELETE
 delete_task = "DELETE FROM tasks WHERE id = ?"
+
+delete_task_completed = "DELETE FROM tasks WHERE completed = 1"
+
+delete_task_uncompleted = "DELETE FROM tasks WHERE completed = 0"
--- main.py
+++ main.py
@@ -3,62 +3,134 @@ from db import main_db
 
 
 def main_page(page: ft.Page):
-    page.title = 'Todo List'
+    page.title = "ToDo List"
     page.theme_mode = ft.ThemeMode.LIGHT
 
-    task_list = ft.Column()
+    task_list = ft.Column(
+        spacing=20,
+        scroll=ft.ScrollMode.AUTO,
+        expand=True
+    )
 
-    def view_task(task_id , task_text):
+    
+    filter_type = {"value": "all"}
 
-        task_field = ft.TextField(value=task_text, expand=True, read_only=True)
+    
+    def load_tasks():
+        task_list.controls.clear()
 
-        def save_edit(_):
-            main_db.update_task(task_id=task_id, new_task = task_field.value)
+        for task_id, task, completed in main_db.get_tasks(filter_type["value"]):
+            task_list.controls.append(
+                view_task(task_id, task, completed)
+            )
+
+        page.update()
+
+   
+    def toggle_task(task_id, is_completed):
+        main_db.update_task(
+            task_id=task_id,
+            completed=int(is_completed)
+        )
+        load_tasks()
+
+    def view_task(task_id, task_text, completed):
+        task_field = ft.TextField(
+            value=task_text,
+            expand=True,
+            read_only=True
+        )
+
+        def save_edit(e):
+            main_db.update_task(
+                task_id=task_id,
+                new_task=task_field.value
+            )
             task_field.read_only = True
+            load_tasks()
+
+        def toggle_edit(e):
+            task_field.read_only = not task_field.read_only
             page.update()
 
-        
-        def del_button(_):
+        def del_button(e):
             main_db.delete_task(task_id=task_id)
-            task_list.controls.remove(task_row)
-            page.update()
+            load_tasks()
 
+        checkbox = ft.Checkbox(
+            value=bool(completed),
+            on_change=lambda e: toggle_task(task_id, e.control.value)
+        )
+
+        edit_button = ft.IconButton(
+            icon=ft.Icons.EDIT,
+            on_click=toggle_edit
+        )
+
+        save_button = ft.IconButton(
+            icon=ft.Icons.SAVE,
+            on_click=save_edit
+        )
 
         delete_button = ft.ElevatedButton('Delete', icon=ft.Icons.DELETE, color=ft.Colors.RED, on_click=del_button)
 
-
-
-        save_button = ft.IconButton(icon=ft.Icons.SAVE, on_click=save_edit)
-
-
-        def enable_edit(_):
-            if task_field.read_only == True:
-                task_field.read_only = False
-            else:
-                task_field.read_only = True
-            page.update()
+        return ft.Row([
+            checkbox,
+            task_field,
+            edit_button,
+            save_button,
+            delete_button
+        ])
 
     
-
-        edit_button = ft.IconButton(icon=ft.Icons.EDIT, on_click=enable_edit)
-        
-        task_row = ft.Row([task_field,edit_button,save_button,delete_button])
-        return task_row
-
     def add_task_flet(e):
-        if text_input.value:
-            task_text = text_input.value.strip()
-            task_id = main_db.add(task=task_text)
-            text_input.value = ""
-            task_list.controls.append(view_task(task_id = task_id, task_text = task_text))
-            page.update()
+        if task_input.value:
+            task_text = task_input.value.strip()
 
-    text_input = ft.TextField(label='Введите задачу',on_submit=add_task_flet)
+            task_id = main_db.add_task(task_text)
 
-    page.add(text_input,task_list)
+            task_input.value = ""
+            load_tasks()
+
+    task_input = ft.TextField(
+        label="Введите задачу",
+        on_submit=add_task_flet
+    )
+
+    
+    def set_filter(value):
+        filter_type["value"] = value
+        load_tasks()
+
+    filter_buttons = ft.Row(
+        [
+            ft.ElevatedButton("Все задачи", on_click=lambda e: set_filter("all")),
+            ft.ElevatedButton("В работе", on_click=lambda e: set_filter("uncompleted")),
+            ft.ElevatedButton("Готово ✅", on_click=lambda e: set_filter("completed")),
+        ],
+        alignment=ft.MainAxisAlignment.SPACE_AROUND
+    )
+
+    def del_completed_button(e):
+        main_db.delete_completed_tasks()
+        load_tasks()
+
+    del_completed_btn = ft.ElevatedButton(
+        "Очистить выполненные",
+        color=ft.Colors.RED,
+        on_click=del_completed_button
+    )
+   
+    page.add(
+        task_input,
+        filter_buttons,
+        del_completed_btn,
+        task_list
+    )
+
+    load_tasks()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main_db.init_db()
     ft.app(target=main_page)
-
